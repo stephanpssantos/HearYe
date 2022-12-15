73f05aa26b5f125e810ad5ca18d0162812085a48
Stephan Santos <stephan.pssantos@gmail.com>
2022-12-15 13:20:36 -0800

    Add login hooks.

---
+++ Shared/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearYe.Shared
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public Guid AadOid { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }   
+    }
+}
