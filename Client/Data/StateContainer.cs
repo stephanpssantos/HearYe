b3be7c76690c1dc1f93010283efb8a506caa8277
Stephan Santos <stephan.pssantos@gmail.com>
2023-01-31 18:45:35 -0800

    Use cascading values to centralize state.

---
+++ Client/Data/StateContainer.cs
@@ -0,0 +1,11 @@
+﻿using HearYe.Shared;
+
+namespace HearYe.Client.Data
+{
+    public class StateContainer
+    {
+        public string? UserDbId { get; set; }
+
+        public IEnumerable<MessageGroup>? UserGroups { get; set; }
+    }
+}
