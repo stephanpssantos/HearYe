ed355197d60d83616bf61c7fc149d39bab6ed093
Stephan Santos <stephan.pssantos@gmail.com>
2023-03-15 13:24:28 -0700

    Add About page.

---
+++ Client/wwwroot/js/custom.js
@@ -0,0 +1,7 @@
+﻿window.customMethods = {
+    carouselTo: function (carouselElementId, index) {
+        let carouselElement = document.querySelector(carouselElementId);
+        let carousel = bootstrap.Carousel.getOrCreateInstance(carouselElement);
+        carousel.to(index);
+    }
+}
